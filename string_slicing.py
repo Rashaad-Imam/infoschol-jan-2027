b07347d1bf3da853a9d5dc54eb3caf86f216a21e
Rashaad Imam <imam5rashaad@gmail.com>
2026-08-05 14:45:47 +0530

    Initial commit

---
+++ string_slicing.py
@@ -0,0 +1,71 @@
+#name="Rashaad"
+#print (name)
+#print (name[0:3])  # prints 'Ras'
+#print (name[0:8:3]) # prints 'haad'
+#print (name[:8:3]) # prints 'Rshd'
+
+# fruit = "orange"
+# print(fruit[0:3])  # prints 'ora'
+# print(fruit[3:6]) # prints 'nge'
+# print(fruit[-6:-3]) # prints 'ora
+# print(fruit[-3:-1]) # prints 'nge'
+# print(fruit)
+
+#list = ["apple", "banana","orange",1,2,3,True]
+
+# slicing
+# print(list[0:3])  # prints ['apple', 'banana', 'orange']
+# print(list[3:6]) # prints [1, 2, 3]
+# print(list[:7:2]) # prints ['apple', 'orange', 2]
+# list[3]="grape"
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 2, 3, True]
+
+# #insert
+# list.insert(4,"kiwi")
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 'kiwi', 2, 3, True]
+
+# #append
+# list.append("false")
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 'kiwi', 2, 3, True, 'false']
+
+# #remove
+# list.remove(2)
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 'kiwi', 3, True, 'false']
+# list.remove(3) 
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 'kiwi', True, 'false']
+
+# #pop
+# list.pop(6)
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 'kiwi', True]
+# print(list) # prints ['apple', 'banana', 'orange', 'grape', 'kiwi', True]
+# list = ["apple", "banana","orange",1,2,3,True]
+
+# #delete
+# # del list[6]
+# # print(list) # prints ['apple', 'banana', 'orange', 1, 2, 3]
+
+# # #clear all
+# # list.clear()
+# # print(list) # prints []
+
+# for i in list:
+#     print(i) # prints each element in the list
+# #for i in list:
+#     #print(i, end=", ") # prints each element in the list separated by a comma
+# for i in list:
+#     print(i, end=" ") # prints each element in the list separated by a space
+
+
+# Name = "Rashaad"
+# print(Name)
+# uppercase_name = Name.upper()
+# print(uppercase_name)  # prints 'RASHAAD'
+# lowercase_name = Name.lower()
+# print(lowercase_name)  # prints 'rashaad'
+
+NAME = "Rashaad"
+print(NAME)
+uppercase_name = NAME.upper()
+print(uppercase_name)  # prints 'RASHAAD'
+lowercase_name = NAME.lower()
+print(lowercase_name)  # prints 'rashaad'
