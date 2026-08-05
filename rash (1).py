011c6913d8ecc55610f39a4a06fddb0fafefabb1
Rashaad Imam <imam5rashaad@gmail.com>
2026-08-05 15:15:28 +0530

    added 2 files

---
+++ rash (1).py
@@ -0,0 +1,35 @@
+# count = 1
+# while count <= 5:
+#     marks = int(input("Please enter your marks: ")) 
+
+#     if marks >= 70:
+#         print ("A")
+#     elif marks >= 55:
+#         print ("B")
+#     elif marks >= 40:
+#         print ("C")
+#     else:
+#         print("F")
+#     count += 1
+
+# num = 1 
+# while (num <= 10):
+#     print(num)
+#     num += 1
+# print ("loop is finished")
+
+# num = 1 
+# while (num <= 10):
+#     print("*"*num)
+#     num += 1
+
+
+# for i in range (1,10):
+#     print (i)
+
+name ="RASHAAD"
+
+for NAME in name:
+    print (NAME)
+   
+
